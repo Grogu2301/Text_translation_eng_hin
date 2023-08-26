16dbe0901237d28a454afe260b3662fbd3af6731
Grogu2301 <nikita.chaturvedi2019@gmail.com>
2023-08-26 13:09:48 +0530

    data validation done

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 
 from texttranslation.logging import logger
 from texttranslation.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
+from texttranslation.pipeline.stage02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME= "Data Ingestion Stage"
 try:
     logger.info(f"{STAGE_NAME} Started")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
+    logger.info(f"{STAGE_NAME} Completed")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f"{STAGE_NAME} Started")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
     logger.info(f"{STAGE_NAME} Completed")
 except Exception as e:
     logger.exception(e)
